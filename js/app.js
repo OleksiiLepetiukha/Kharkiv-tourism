396e2ee0aeceaec0f075b59739e7af3d9132e1a9
DESKTOP-IV1O8UM\Oleksii <alekseylepetukha@icloud.com>
2023-06-12 08:53:41 +0100

    added swiper

--- js/app.js
+++ js/app.js
@@ -18,3 +18,15 @@ function drop() {
     }
 }
 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
