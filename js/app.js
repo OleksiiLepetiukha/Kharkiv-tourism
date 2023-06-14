649fdd62cddbe6d0aa6e08368d3785b271c93a22
DESKTOP-IV1O8UM\Oleksii <alekseylepetukha@icloud.com>
2023-06-14 09:17:46 +0100

    added coments and form validation function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// function for burger menu in header
+
 function burger() {
     const navbar = document.querySelector(".navigation");
     if (navbar.style.display === "flex") {
@@ -7,6 +9,8 @@ function burger() {
     }
 }
 
+// function for dropdown menu on dining page
+
 function drop(event) {
     const par = event.target.parentNode;
     const info = par.querySelector('.restaurant-info');
@@ -14,6 +18,8 @@ function drop(event) {
     const btn = par.querySelector('.restaurant-button')
     btn.innerHTML = (info.classList.length > 1) ? 'Less...' : 'More..';
 }
+
+// function for slider on attractions page
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -27,3 +33,82 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
 });
+
+// form validation function for contact form on contacts page
+
+const form = document.querySelector('.callback-form');
+const nameInput = document.querySelector('#fullName');
+const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
+const nameError = document.querySelector('#fullName + .field-error');
+const emailError = document.querySelector('#email + .field-error');
+const phoneError = document.querySelector('#phone + .field-error');
+
+
+function validateName() {
+    if (nameInput.validity.valid) {
+        nameError.textContent = '';
+    } else {
+        showNameError();
+    }
+}
+
+function showNameError() {
+    nameError.textContent = 'Please enter your name';
+}
+
+function validateEmail() {
+    if (emailInput.validity.valid) {
+        emailError.textContent = '';
+    } else {
+        showEmailError();
+    }
+}
+
+function showEmailError() {
+    if (emailInput.validity.valueMissing) {
+        emailError.textContent = 'Please enter your email address';
+    } else if(emailInput.validity.typeMismatch) {
+        emailError.textContent = 'Your email address does not appear to be correct';
+    }
+}
+
+function validatePhone() {
+    if (phoneInput.validity.valid) {
+        nameError.textContent = '';
+    } else {
+        showPhoneError();
+    }
+}
+
+function showPhoneError() {
+    phoneError.textContent = 'Please enter your phone number';
+}
+
+function validateForm(event) {
+    let formHasErrors = false;
+    if (!nameInput.validity.valid) {
+        formHasErrors = true;
+        showNameError();
+    }
+
+    if(!emailInput.validity.valid){
+        formHasErrors = true;
+        showEmailError();
+    }
+
+    if(!phoneInput.validity.valid){
+        formHasErrors = true;
+        showPhoneError();
+    }
+
+    if(formHasErrors){
+        event.preventDefault();
+    }
+}
+
+nameInput.addEventListener('input', validateName);
+emailInput.addEventListener('input', validateEmail);
+phoneInput.addEventListener('input', validatePhone);
+
+form.addEventListener('submit', validateForm);
